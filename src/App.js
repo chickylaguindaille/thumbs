f204ec083270387e047a7eefef4a78e18eea7aee
TarikAguen <98916020+TarikAguen@users.noreply.github.com>
2024-10-20 23:35:41 +0200

    set up axeptio

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route, Routes, useLocation, Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import Header from './components/Header';
-import Sidebar from './components/Sidebar';
-import EventsPage from './pages/EventsPage';
-import EventPage from './pages/EventPage';
-import MessagesPage from './pages/MessagesPage';
-import ChatPage from './pages/ChatPage';
-import ProfilePage from './pages/ProfilePage';
-import AssociationsPage from './pages/AssociationsPage';
-import AssociationPage from './pages/AssociationPage';
-import LoginPage from './pages/LoginPage';
-import ForgotPasswordPage from './pages/ForgotPasswordPage';
-import ResetPasswordPage from './pages/ResetPasswordPage';
-import SignupPage from './pages/SignupPage';
-import TermsPage from './pages/TermsPage';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+  Navigate,
+} from "react-router-dom";
+import { useSelector } from "react-redux";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import Axeptio from "./components/Axeptio";
+import EventsPage from "./pages/EventsPage";
+import EventPage from "./pages/EventPage";
+import MessagesPage from "./pages/MessagesPage";
+import ChatPage from "./pages/ChatPage";
+import ProfilePage from "./pages/ProfilePage";
+import AssociationsPage from "./pages/AssociationsPage";
+import AssociationPage from "./pages/AssociationPage";
+import LoginPage from "./pages/LoginPage";
+import ForgotPasswordPage from "./pages/ForgotPasswordPage";
+import ResetPasswordPage from "./pages/ResetPasswordPage";
+import SignupPage from "./pages/SignupPage";
+import TermsPage from "./pages/TermsPage";
 
 // Composant pour protéger les routes
 const ProtectedRoute = ({ element }) => {
   // const dispatch = useDispatch();
-  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
   // const token = useSelector(state => state.auth.token); // Assurez-vous d'avoir le token dans votre state
 
   return isAuthenticated ? element : <Navigate to="/login" />;
@@ -30,18 +37,18 @@ function MainLayout() {
   const location = useLocation();
   const [isSidebarOpen, setIsSidebarOpen] = useState(window.innerWidth >= 768);
   // const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
-  
+
   useEffect(() => {
     const handleResize = () => {
       if (window.innerWidth >= 768) {
         setIsSidebarOpen(true);
       }
     };
-    
+
     handleResize();
-    window.addEventListener('resize', handleResize);
-    
-    return () => window.removeEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   const toggleSidebar = () => {
@@ -50,7 +57,12 @@ function MainLayout() {
     }
   };
 
-  const isPublicRoute = location.pathname === '/login' || location.pathname === '/forgot-password' || location.pathname.startsWith('/reset-password') || location.pathname === '/signup' || location.pathname === '/terms';
+  const isPublicRoute =
+    location.pathname === "/login" ||
+    location.pathname === "/forgot-password" ||
+    location.pathname.startsWith("/reset-password") ||
+    location.pathname === "/signup" ||
+    location.pathname === "/terms";
 
   return (
     <div className="flex flex-col md:flex-row">
@@ -59,7 +71,11 @@ function MainLayout() {
         <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
       )}
 
-      <div className={`flex-1 transition-all duration-300 ${isSidebarOpen && !isPublicRoute ? 'md:ml-64' : 'md:ml-0'}`}>
+      <div
+        className={`flex-1 transition-all duration-300 ${
+          isSidebarOpen && !isPublicRoute ? "md:ml-64" : "md:ml-0"
+        }`}
+      >
         {/* Header */}
         {!isPublicRoute && (
           <Header toggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} />
@@ -68,17 +84,44 @@ function MainLayout() {
         {/* Routes */}
         <Routes>
           <Route path="/terms" element={<TermsPage />} />
-          <Route path="/" element={<ProtectedRoute element={<EventsPage />} />} /> 
-          <Route path="/events" element={<ProtectedRoute element={<EventsPage />} />} />
-          <Route path="/events/:id" element={<ProtectedRoute element={<EventPage />} />} />
-          <Route path="/messages" element={<ProtectedRoute element={<MessagesPage />} />} />
-          <Route path="/messages/:id" element={<ProtectedRoute element={<ChatPage />} />} />
-          <Route path="/profile/:id" element={<ProtectedRoute element={<ProfilePage />} />} />
-          <Route path="/associations" element={<ProtectedRoute element={<AssociationsPage />} />} />
-          <Route path="/association/:id" element={<ProtectedRoute element={<AssociationPage />} />} />
+          <Route
+            path="/"
+            element={<ProtectedRoute element={<EventsPage />} />}
+          />
+          <Route
+            path="/events"
+            element={<ProtectedRoute element={<EventsPage />} />}
+          />
+          <Route
+            path="/events/:id"
+            element={<ProtectedRoute element={<EventPage />} />}
+          />
+          <Route
+            path="/messages"
+            element={<ProtectedRoute element={<MessagesPage />} />}
+          />
+          <Route
+            path="/messages/:id"
+            element={<ProtectedRoute element={<ChatPage />} />}
+          />
+          <Route
+            path="/profile/:id"
+            element={<ProtectedRoute element={<ProfilePage />} />}
+          />
+          <Route
+            path="/associations"
+            element={<ProtectedRoute element={<AssociationsPage />} />}
+          />
+          <Route
+            path="/association/:id"
+            element={<ProtectedRoute element={<AssociationPage />} />}
+          />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-          <Route path="/reset-password/:token" element={<ResetPasswordPage />} />
+          <Route
+            path="/reset-password/:token"
+            element={<ResetPasswordPage />}
+          />
           <Route path="/signup" element={<SignupPage />} />
         </Routes>
       </div>
@@ -89,6 +132,7 @@ function MainLayout() {
 function App() {
   return (
     <Router>
+      <Axeptio />
       <MainLayout />
     </Router>
   );
